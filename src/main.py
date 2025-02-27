8031e475bcf8e46c2a576e443198fa0750c03dd6
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-02-27 14:34:25 +0000

    more pseudocode

--- src/main.py
+++ src/main.py
@@ -100,15 +100,15 @@ def main():
         user_action = game.handle_events()
  
         # here we want to add control for the start button
-        if game.state == "start" and user_action == "clicked":
+        if game.state == "start" and user_action == "click_continue":
             game.state = "map"
             game.set
-        elif game.state == "map" and user_action == "clicked":
+        elif game.state == "map" and user_action == "click_pin":
             # here we want to transfer to the screem of where the clicked pin is
             # game.state = "encounter"
             pass
         elif game.state == "encounter":
-            encounter(animals, game)
+            encounter(animals, game, player)
             game.state = "map"
         game.clock.tick(30) # 30 frames per second
 
@@ -132,7 +132,12 @@ def encounter(animals, game):
     #   # if animal.escapes()
     #   #   # return
     # spawn the animal
-    # dialogue and minigame
+    # dialogue
+    # if minigame()
+    #   # player.exp += animal.get_exp()
+    #   # check if level up
+    #   #   # player.level_up()
+    #   # player.shells += animal.get_shells()
     # return
 
     pass
